8ac6a00d72fbe270d569802f4adb9783f8959696
NandanaHasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2024-10-29 15:14:50 +0530

    Add files via upload

---
+++ multiplication table.py
@@ -0,0 +1,9 @@
+'''
+Author:Nandana Hasheed
+Date:29-10-2024
+Python program to print multiplication table upto 12
+'''
+num=int(input("Enter the number"))
+for i in range(1,13):
+    mul=num*i
+    print(num,"*",i,"=",mul)
