f25b475d060a918b07530fd0b17a608205cfe2bd
NandanaHasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2024-10-29 15:53:58 +0530

    Update multiplication table.py

--- multiplication table.py
+++ multiplication table.py
@@ -6,4 +6,4 @@ Python program to print multiplication table upto 12
 num=int(input("Enter the number"))
 for i in range(1,13):
     mul=num*i
-    print(num,"*",i,"=",mul)
+    print(num,"*",i,"\t","=",mul)
